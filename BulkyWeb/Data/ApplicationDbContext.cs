7ea17d3caca2e64826bd9b2bdbfa15a11d7dc32e
lakshminiranjan <lakshmi.niranjan2019@vitstudent.ac.in>
2025-10-07 19:39:22 +0530

    Till now learnt how to create a table and how to connect to database from Entity Framework Core

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        //ctor-> tab tab-new constructor snippet
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+        [Key]//Primary Key
+        public int Id { get; set; }
+        [Required]//Not null
+        public string Name { get; set; }
+        public int DispalyOrder { get; set; }
+    }
+}
