702242d73d86b7fdf4d8e702c95416de16f4d23a
lakshminiranjan <lakshmi.niranjan2019@vitstudent.ac.in>
2025-10-21 21:09:07 +0530

    Complete Course Done as of Youtube

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -10,11 +10,19 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+
+
 builder.Services.AddRazorPages();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser,IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+builder.Services.ConfigureApplicationCookie(options => {
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
-
+builder.Services.AddScoped<Microsoft.AspNetCore.Identity.UI.Services.IEmailSender, Bulky.Utility.EmailSender>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
